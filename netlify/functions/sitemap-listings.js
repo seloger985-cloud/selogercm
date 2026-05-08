5e820e7f96616985836b883cbdbab8f7fbc2a1d0
seloger985-cloud <seloger985@gmail.com>
2026-05-08 04:38:49 +0100

    Node Netlify_sitemap

--- netlify/functions/sitemap-listings.js
+++ netlify/functions/sitemap-listings.js
@@ -55,10 +55,11 @@ ${urls}
     };
 
   } catch (err) {
-    console.error('sitemap-listings error:', err);
+    console.error('sitemap-listings error:', err.message || err);
     return {
       statusCode: 500,
-      body: `<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"></urlset>`,
+      headers: { 'Content-Type': 'text/plain' },
+      body: `sitemap-listings error: ${err.message || err}`,
     };
   }
 };
